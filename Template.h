2eb77980538e9eafd61812c4a607d88b55f2b9d9
Jim-CPP <jim.tech@atomicmail.io>
2026-08-07 13:41:37 +0100

    Simple Window

---
+++ Template.h
@@ -0,0 +1,19 @@
+// Template.h
+
+#pragma once
+
+#include <windows.h>
+
+#define MAIN_WINDOW_CLASS_NAME													"Main Template Window Class"
+
+#define MAIN_WINDOW_CLASS_STYLE													0
+#define MAIN_WINDOW_CLASS_ICON_NAME												IDI_APPLICATION
+#define MAIN_WINDOW_CLASS_ICON													LoadIcon( NULL, MAIN_WINDOW_CLASS_ICON_NAME )
+#define MAIN_WINDOW_CLASS_CURSOR												LoadCursor( NULL, IDC_ARROW )
+#define MAIN_WINDOW_CLASS_BACKGROUND											( HBRUSH )( COLOR_WINDOW + 1 )
+#define MAIN_WINDOW_CLASS_MENU_NAME												NULL
+#define MAIN_WINDOW_CLASS_ICON_SMALL											LoadIcon( NULL, MAIN_WINDOW_CLASS_ICON_NAME )
+
+#define MAIN_WINDOW_EXTENDED_STYLE												WS_EX_CLIENTEDGE
+#define MAIN_WINDOW_TEXT														"Template"
+#define MAIN_WINDOW_STYLE														WS_OVERLAPPEDWINDOW
